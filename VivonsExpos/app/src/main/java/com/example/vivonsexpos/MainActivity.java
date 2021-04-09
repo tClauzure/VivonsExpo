f7fca8c1bf4e8f9d9b33a72685c7712ce93a3779
louloucongo <adrien.frossard3390@gmail.com>
2021-04-09 14:49:14 +0200

    menuConnexion
    je suis beau

--- VivonsExpos/app/src/main/java/com/example/vivonsexpos/MainActivity.java
+++ VivonsExpos/app/src/main/java/com/example/vivonsexpos/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.vivonsexpos;
 
+
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
-
+import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button buttonMenuConnexion = (Button) findViewById(R.id.buttonConnexion);
+        buttonMenuConnexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, menuConnexion.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
---
+++ VivonsExpos/app/src/main/java/com/example/vivonsexpos/menuConnexion.java
@@ -0,0 +1,6 @@
+package com.example.vivonsexpos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class menuConnexion extends AppCompatActivity {
+}
