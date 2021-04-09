e1d7a0e8500c50f68e6c80326a92e372a9d5e123
laura <laura.bojon@gmail.com>
2021-04-09 14:30:43 +0200

    mainActivity

--- VivonsExpos/app/src/main/java/com/example/vivonsexpos/MainActivity.java
+++ VivonsExpos/app/src/main/java/com/example/vivonsexpos/MainActivity.java
@@ -1,8 +1,22 @@
 package com.example.vivonsexpos;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.IOException;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import android.os.Bundle;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +24,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button buttonMenuConnexion = (Button) findViewById(R.id.buttonConnexion);
+        buttonMenuConnexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MenuConnexion.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
