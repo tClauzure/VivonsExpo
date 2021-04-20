7b2e195dc7682175bb1b1a99d4526d8660e86a78
tclauzure <theo.clauzure@outlook.fr>
2021-04-20 16:35:55 +0200

    menu staff

--- VivonsExpos/app/src/main/java/com/example/vivonsexpos/menuStaff.java
+++ VivonsExpos/app/src/main/java/com/example/vivonsexpos/menuStaff.java
@@ -1,6 +1,27 @@
 package com.example.vivonsexpos;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class menuStaff extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button buttonAffectationStand = (Button) findViewById(R.id.btnAffectationStand);
+        buttonAffectationStand.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(menuStaff.this, affectationStand.class);
+                startActivity(intent);
+            }
+        });
+
+    }
 }
--- VivonsExpos/app/src/main/java/com/example/vivonsexpos/pageConnex.java
+++ VivonsExpos/app/src/main/java/com/example/vivonsexpos/pageConnex.java
@@ -5,9 +5,9 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class pageConnex extends AppCompatActivity {
-    @Override
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_connex);
-    }
+    }*/
 }
