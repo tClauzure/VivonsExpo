7e54147290150098acd8fc79061d3cd66883e3eb
louloucongo <adrien.frossard3390@gmail.com>
2021-04-20 16:52:41 +0200

    20/04

--- VivonsExpos/app/src/main/java/com/example/vivonsexpos/menuVisiteur.java
+++ VivonsExpos/app/src/main/java/com/example/vivonsexpos/menuVisiteur.java
@@ -14,22 +14,29 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class menuVisiteur extends AppCompatActivity {
-  /*  @Override
+   @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visiteur);
+        final Button buttonMenuBackVisiteur = (Button) findViewById(R.id.buttonBack2);
+        buttonMenuBackVisiteur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(menuVisiteur.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        try{
+      /* try{
             final JSONObject vivonsexpos =new JSONObject (getIntent().getStringExtra("visiteur"));
             final TextView textRecherche = findViewById(R.id.textRecherche);
             String texteRecherche = vivonsexpos.getString("typeutilisateur");
             textRecherche.setText(texteRecherche);
-
-
-
-
         }
+        catch(JSONException e){
+            Toast.makeText(menuVisiteur.this,"message!",Toast.LENGTH_SHORT).show();
+       }*/
 
-    }*/
+    }
 
 }
